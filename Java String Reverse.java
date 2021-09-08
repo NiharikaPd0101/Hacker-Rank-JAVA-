47bcc54c3daaea704a38903153dbf5af3abe8112
NiharikaPd0101 <80149490+NiharikaPd0101@users.noreply.github.com>
2021-09-08 23:43:57 +0530

    Create Java String Reverse.java

---
+++ Java String Reverse.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class JavaStringReverse {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+        sc.close();
+        boolean found = true;
+        for (int i = 0; i < A.length() / 2; i++) {
+            if (A.charAt(i) != A.charAt(A.length() - 1 - i)) {
+                found = false;
+                break;
+            }
+        }
+        System.out.println(found ? "Yes" : "No");
+    }
+}
