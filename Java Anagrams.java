d4aafa2e77ed2abad9851bef149427aa69b7f4c0
NiharikaPd0101 <80149490+NiharikaPd0101@users.noreply.github.com>
2021-09-08 23:44:48 +0530

    Create Java Anagrams.java

---
+++ Java Anagrams.java
@@ -0,0 +1,23 @@
+
+
+    static boolean isAnagram(String a, String b) {
+        if (a.length() != b.length()) {
+            return false;
+        } else {
+            for (int i = 0; i < a.length(); i++) {
+                char ch = a.toLowerCase().charAt(i);
+                b = b.toLowerCase();
+                if (b.indexOf(ch) != -1) {
+                    b = b.replaceFirst(ch + "", "");
+                } else {
+                    return false;
+                }
+            }
+            return b.length() == 0;
+        }
+    }
+
+    
+
+
+
