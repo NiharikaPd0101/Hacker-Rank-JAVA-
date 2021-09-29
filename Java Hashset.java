5a2e2c8c17c3b0e887ba607ddf8680b2680ec1f0
NIHARIKA PRASAD <80149490+NiharikaPd0101@users.noreply.github.com>
2021-09-29 21:10:40 +0530

    Create Java Hashset.java

---
+++ Java Hashset.java
@@ -0,0 +1,8 @@
+
+        Set<String> set = new HashSet<String>();
+        for (int i = 0; i < t; i++) {
+            set.add(pair_left[i] + "_" + pair_right[i]);
+            System.out.println(set.size());
+
+        }
+       
