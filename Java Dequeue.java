7786d29878556a40fa73f80f40696decf9a3560a
NIHARIKA PRASAD <80149490+NiharikaPd0101@users.noreply.github.com>
2021-09-30 21:44:51 +0530

    Create Java Dequeue.java

---
+++ Java Dequeue.java
@@ -0,0 +1,17 @@
+    import java.util.*;
+    public class test {
+        public static void main(String[] args) {
+            Scanner in = new Scanner(System.in);
+            Deque deque = new ArrayDeque<>();
+            int n = in.nextInt();
+            int m = in.nextInt();
+
+            for (int i = 0; i < n; i++) {
+                int num = in.nextInt();
+               
+            }
+        }
+    }
+
+
+
