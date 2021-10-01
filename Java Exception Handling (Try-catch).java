586bb1f57d2fce8672e4a54eef30a62c53928f3c
NIHARIKA PRASAD <80149490+NiharikaPd0101@users.noreply.github.com>
2021-10-01 22:37:05 +0530

    Create Java Exception Handling (Try-catch).java

---
+++ Java Exception Handling (Try-catch).java
@@ -0,0 +1,20 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class JavaExceptionHandlingTryCatch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            int z = x / y;
+            System.out.println(z);
+        } catch (ArithmeticException e) {
+            System.out.println(e);
+        } catch (InputMismatchException e) {
+            System.out.println(e.getClass().getName());
+        } finally {
+            sc.close();
+        }
+    }
+}
